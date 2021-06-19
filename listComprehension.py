50d6a2b2b356cd0c1a5f4510e99e080ffe59cf3e
AnanduRamachandran <ananduyou@gmail.com>
2021-06-19 10:52:09 +0530

    Comprehension updated

--- listComprehension.py
+++ listComprehension.py
@@ -21,4 +21,35 @@ print (list(my_list)) #my_list was a map
 my_list = [n for n in nums if n%2 == 0]
 print(my_list)
 
-#Same with  i
+# Using a filter + lambda
+my_list = filter(lambda n: n%2 == 0, nums)
+print my_list
+
+names = ['Bruce', 'Clark', 'Peter', 'Logan', 'Wade']
+heros = ['Batman', 'Superman', 'Spiderman', 'Wolverine', 'Deadpool']
+# print zip(names, heros) <-- To match both lists one to one
+
+# I want a dict{'name': 'hero} for each name, hero in zip(name, heros)
+
+#Using for loop
+my_dict = {}
+for name, hero in zip(name, heros):
+     my_dict{name} = hero
+print my_dict
+
+#Using dictionary comprehension
+my_dict = {name: hero for name, hero in zip (name, heros)}
+print(my_dict)
+
+#Set comprehension
+
+# Using for loop
+nums = [1,1,2,1,3,4,3,4,5,5,6,7,8,7,9,9]
+my_set = set()
+for n in nums:
+     my_set.add(n)
+print my_set
+
+#Using set comprehension
+my_set = {n for n in nums}
+print my_set
